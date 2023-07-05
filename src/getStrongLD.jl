d9eb50e028d42af46155d1fceee686f70a99e83e
SamuelMathieu-code <samuelmathieu@zoho.com>
2023-07-05 17:18:38 -0400

    getStrongLD

--- src/getStrongLD.jl
+++ src/getStrongLD.jl
@@ -3,10 +3,10 @@ using Folds
 
 
 function getStrongLD(ref_genotypes::SnpData, 
-                     snps::AbstractVector{<:Tuple{Integer, Integer}}, 
+                     snps::AbstractVector{<:Tuple{Integer, Integer}}; 
                      r2_tresh::Float64 = 0.8,
                      window::Int64 = 500000,
-                     formated::Bool = true)
+                     formated::Bool = false)
     if !formated
         formatSnpData!(ref_genotypes)
     end
@@ -20,15 +20,20 @@ function getStrongLD(ref_genotypes::SnpData,
         snps_indx[i] = j
     end
     kept_input = snps_indx .> 0
-    kept_indx = snps_indx[kept_input]
+    snps_indx = snps_indx[kept_input]
+    kept_idx = ref_genotypes.snp_info.idx[snps_indx]
+    kept_chr = ref_genotypes.snp_info.chromosome[snps_indx]
+    kept_pos = ref_genotypes.snp_info.position[snps_indx]
 
-    res = Vector{Vector{Int}}(undef, length(kept_indx))
-    for (i, j) in enumerate(kept_indx)
-        idx_v_b = ref_genotypes.snp_info.chromosome .== snps[i][1] .& abs.(ref_genotypes.snp_info.position .- snps[i][2]) .< window
+    res = Vector{Vector{Int}}(undef, length(kept_idx))
+    for i in eachindex(kept_idx)
+        t1 = ref_genotypes.snp_info.chromosome .== kept_chr[i]
+        t2 = abs.(ref_genotypes.snp_info.position .- kept_pos[i]) .< window
+        idx_v_b = t1 .& t2
         indexes = ref_genotypes.snp_info.idx[idx_v_b]
-        corr = [ld_r²(s, kept_indx[i], ref_genotypes.snparray) for s in indexes]
-        kept = corr .> r2_tresh
-        res[i] = indexes[kept]
+        kept = findall(idx_v_b)
+        corr = [ld_r²(s, kept_idx[i], ref_genotypes.snparray) for s in indexes]
+        res[i] = kept[corr .> r2_tresh]
     end
     res2 = Folds.reduce(vcat, res, init = [])
 
@@ -41,51 +46,25 @@ function getStrongLD(ref_genotypes::AbstractVector{SnpData},
                      snps::AbstractVector{<:Tuple{Integer, Integer}}; 
                      r2_tresh::Float64 = 0.8,
                      window::Int64 = 500000,
-                     formated::Bool = true)
-    if !formated
-    formatSnpData!.(ref_genotypes)
-    end
-
-    snps_indx = Vector{Int}(undef, size(snps, 1))
-    snps_chr = Vector{Int}(undef, size(snps, 1))
-    @threads for (i, chr_pos_sing) in collect(enumerate(snps))
-        local j = searchsortedfirst(ref_genotypes[chr_pos_sing[1]].snp_info.chr_pos, chr_pos_sing)
-        if j > lastindex(ref_genotypes[chr_pos_sing[1]].snp_info.chr_pos) || ref_genotypes[chr_pos_sing[1]].snp_info.chr_pos[j] != chr_pos_sing
-            j = -1
-        end
-        snps_indx[i] = j
-        snps_chr[i] = snps[i][1]
-    end
-    kept_input = snps_indx .> 0
-    kept_snps = snps[kept_input]
-    kept_indx = snps_indx[kept_input]
-    kept_chr = snps_chr[kept_input]
-  
-
-    res = Vector{Vector{Int}}(undef, length(kept_indx))
-    res_chrpos = Vector{Vector{Tuple{Int8, Int32}}}(undef, length(kept_indx))
-    @threads for i in eachindex(kept_indx, kept_chr, res)
-        idx_v_b = abs.(ref_genotypes[kept_chr[i]].snp_info.position .- kept_snps[i][2]) .< window
-        indexes = ref_genotypes[kept_chr[i]].snp_info.idx[idx_v_b]
-        corr = [ld_r²(s, kept_indx[i], ref_genotypes[kept_chr[i]].snparray) for s in indexes]
-        kept = corr .> r2_tresh
-        res[i] = indexes[kept]
-    end
+                     formated::Bool = false)
     
-    @threads for i in eachindex(kept_chr, res, kept_indx)
-        res_chrpos[i] = ref_genotypes[kept_chr[i]].snp_info.chr_pos[res[i]]
+    a = Vector{Vector{Tuple{Int8, Int}}}(repeat([[]], 22))
+    for c in snps
+        push!(a[c[1]], c)
     end
-
-    return Folds.reduce(vcat, res_chrpos, init = [])
-
+    r = Vector{Tuple{Int8, Int64}}([])
+    for i in 1:22
+        append!(r, getStrongLD(ref_genotypes[i], a[i], r2_tresh = r2_tresh, window = window, formated = formated))
+    end
+    return r
 end
 
 
 function getStrongLD(ref_genotypes::SnpData, 
-                     snps::AbstractVector{<:AbstractString}, 
+                     snps::AbstractVector{<:AbstractString};
                      r2_tresh::Float64 = 0.8,
                      window::Int64 = 500000,
-                     formated::Bool = true)
+                     formated::Bool = false)
     if !formated
         formatSnpData!(ref_genotypes, :snpid)
     end
@@ -99,63 +78,41 @@ function getStrongLD(ref_genotypes::SnpData,
         snps_indx[i] = j
     end
     kept_input = snps_indx .> 0
-    kept_indx = snps_indx[kept_input]
+    snps_indx = snps_indx[kept_input]
+    kept_idx = ref_genotypes.snp_info.idx[snps_indx]
+    kept_chr = ref_genotypes.snp_info.chromosome[snps_indx]
+    kept_pos = ref_genotypes.snp_info.position[snps_indx]
 
-    res = Vector{Vector{Int}}(undef, length(kept_indx))
-    for (i, j) in enumerate(kept_indx)
-        chr, pos = ref_genotypes.snp_info.chromosome[j], ref_genotypes.snp_info.position[j]
-        idx_v_b = ref_genotypes.snp_info.chromosome .== chr .& abs.(ref_genotypes.snp_info.position .- pos) .< window
+
+    res = Vector{Vector{Int}}(undef, length(kept_idx))
+    for i in eachindex(kept_idx)
+        chr, pos = kept_chr[i], kept_pos[i]
+        t1 = ref_genotypes.snp_info.chromosome .== chr
+        t2 = abs.(ref_genotypes.snp_info.position .- pos) .< window
+        idx_v_b = t1 .& t2
         indexes = ref_genotypes.snp_info.idx[idx_v_b]
-        corr = [ld_r²(s, kept_indx[i], ref_genotypes.snparray) for s in indexes]
-        kept = corr .> r2_tresh
-        res[i] = indexes[kept]
+        kept = findall(idx_v_b)
+        corr = [ld_r²(s, kept_idx[i], ref_genotypes.snparray) for s in indexes]
+        res[i] = kept[corr .> r2_tresh]
     end
-    res2 = Folds.reduce(vcat, res)
+    res2 = Folds.reduce(vcat, res, init = [])
 
-    return ref_genotypes.snp_info.chr_pos[res2]
+    return ref_genotypes.snp_info.snpid[res2]
     
 end
 
 
 function getStrongLD(ref_genotypes::AbstractVector{SnpData}, 
-                     snps::AbstractVector{<:AbstractString};
-                     r2_tresh::Float64 = 0.8,
-                     window::Int64 = 500000,
-                     formated::Bool = true)
-    if !formated
-    formatSnpData!.(ref_genotypes, :snpid)
+    snps::AbstractVector{<:AbstractString}; 
+    r2_tresh::Float64 = 0.8,
+    window::Int64 = 500000,
+    formated::Bool = false)
+
+    res = Vector{String}([])
+    for i in eachindex(ref_genotypes)
+        append!(res, getStrongLD(ref_genotypes[i], snps, r2_tresh = r2_tresh, window = window, formated = formated))
     end
 
-    snps_indx = Vector{Int}(undef, size(snps, 1))
-    snps_chr = Vector{Int}(undef, size(snps, 1))
-    @threads for (i, chr_pos_sing) in collect(enumerate(snps))
-        local j = searchsortedfirst(ref_genotypes[chr_pos_sing[1]].snp_info.snpid, chr_pos_sing)
-        if j > lastindex(ref_genotypes[chr_pos_sing[1]].snp_info.snpid) || ref_genotypes[chr_pos_sing[1]].snp_info.snpid[j] != chr_pos_sing
-            j = -1
-        end
-        snps_indx[i] = j
-        snps_chr[i] = snps[i][1]
-    end
-    kept_input = snps_indx .> 0
-    kept_indx = snps_indx[kept_input]
-    kept_chr = snps_chr[kept_input]
-  
-
-    res = Vector{Vector{Int}}(undef, length(kept_indx))
-    res_chrpos = Vector{Vector{Tuple{Int8, Int32}}}(undef, length(kept_indx))
-    @threads for i in eachindex(kept_indx, kept_chr, res)
-        pos = ref_genotypes[kept_chr[i]].snp_info.position[kept_indx[i]]
-        idx_v_b = abs.(ref_genotypes[kept_chr[i]].snp_info.position .- pos) .< window
-        indexes = ref_genotypes[kept_chr[i]].snp_info.idx[idx_v_b]
-        corr = [ld_r²(s, kept_indx[i], ref_genotypes[kept_chr[i]].snparray) for s in indexes]
-        kept = corr .> r2_tresh
-        res[i] = indexes[kept]
-    end
-    
-    @threads for i in eachindex(kept_chr, res, kept_indx)
-        res_chrpos[i] = ref_genotypes[kept_chr[i]].snp_info.snpid[res[i]]
-    end
-
-    return Folds.reduce(vcat, res_chrpos, init = [])
-
+    return unique(res)
 end
+
